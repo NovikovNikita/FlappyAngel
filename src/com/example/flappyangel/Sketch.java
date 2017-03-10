2ec25cf6364521e1e33e5c6536f72cb92aaefcdd
Nikita <nothanks000@gmail.com>
2017-03-10 15:59:22 +0300

    staring project

---
+++ src/com/example/flappyangel/Sketch.java
@@ -0,0 +1,17 @@
+package com.example.flappyangel;
+
+import processing.core.PApplet;
+import processing.core.PFont;
+import processing.core.PImage;
+
+public class Sketch extends PApplet {
+
+    public void settings() { size(800, 600); }
+    public void setup(){
+
+    }
+
+    public void draw() {
+
+    }
+}
