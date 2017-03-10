fafc5336b86c805b776f7180861ac3d5c6a48fcb
Nikita <nothanks000@gmail.com>
2017-03-10 16:51:57 +0300

    staring project

--- src/com/example/flappyangel/Main.java
+++ src/com/example/flappyangel/Main.java
@@ -4,7 +4,7 @@ import processing.core.PApplet;
 
 public class Main {
 
-    private final static String[] OPTIONS = new String[] {"-present", "Sketch"};
-    public static void main(String[] args) { PApplet.main(OPTIONS); }
+    private final static String[] OPTIONS = new String[] {"--present", "Sketch"};
+    public static void main(String ... args) { PApplet.main(OPTIONS); }
 
 }
--- src/com/example/flappyangel/Sketch.java
+++ src/com/example/flappyangel/Sketch.java
@@ -5,13 +5,22 @@ import processing.core.PFont;
 import processing.core.PImage;
 
 public class Sketch extends PApplet {
+    float xpos = 200f;
+    float ypos = 50f;
+    float vy = 0f;
+    float gravity = 0.98f;
 
     public void settings() { size(800, 600); }
     public void setup(){
-
+        smooth();
+        noStroke();
     }
 
     public void draw() {
-
+        background(0);
+        fill(255);
+        rect(xpos, ypos, 40, 40);
+        vy += gravity;
+        ypos += vy;
     }
 }
