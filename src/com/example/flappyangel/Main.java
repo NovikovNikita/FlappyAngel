ac6500969da0ceff4b3167e8e35bf6251c586b71
Nikita <nothanks000@gmail.com>
2017-03-10 17:41:59 +0300

    game already done

--- src/com/example/flappyangel/Main.java
+++ src/com/example/flappyangel/Main.java
@@ -5,6 +5,9 @@ import processing.core.PApplet;
 public class Main {
 
     private final static String[] OPTIONS = new String[] {"--present", "Sketch"};
-    public static void main(String ... args) { PApplet.main(OPTIONS); }
+
+    public static void main(String ... args){
+        PApplet.main(OPTIONS);
+    }
 
 }
--- src/com/example/flappyangel/Sketch.java
+++ src/com/example/flappyangel/Sketch.java
@@ -8,19 +8,131 @@ public class Sketch extends PApplet {
     float xpos = 200f;
     float ypos = 50f;
     float vy = 0f;
-    float gravity = 0.98f;
+    float gravity = 0.5f;
+    PImage bird;
+    PImage background;
+    PImage wall;
+    PImage reset;
+    int count = 300;
+    WallPair[] walls = new WallPair[4];
+    boolean lose = false;
+    int score = 0;
+    PFont flappyFont;
+    float[] wallOffset = new float[4];
 
     public void settings() { size(800, 600); }
     public void setup(){
         smooth();
         noStroke();
+        bird = loadImage("bird1.png");
+        background = loadImage("back.png");
+        wall = loadImage("truba.png");
+        flappyFont = createFont("04B_19__.TTF", 38.0f, true);
+        fill(255);
+        textFont(flappyFont);
+        textAlign(LEFT, TOP);
+        for (int i = 0; i < wallOffset.length; i++) {
+            wallOffset[i] = random(-100, 100);
+        }
     }
 
     public void draw() {
-        background(0);
-        fill(255);
-        rect(xpos, ypos, 40, 40);
+        if (!lose) {
+            count++;
+        }
+        drawBackgroundAndWalls(count%width);
+        image(bird, xpos, ypos);
         vy += gravity;
         ypos += vy;
+        if (ypos > height - 50) {
+            lose = true;
+        }
+        checkTouch();
+        text("SCORE: "+score, width - 200, 30);
+        if (lose) {
+            imageMode(CENTER);
+            image(reset, width/2, height/2);
+            imageMode(CORNER);
+        }
+    }
+
+    void drawBackgroundAndWalls(int offset) {
+        background(0);
+        image(background, -offset, 0);
+        image(background, width-offset, 0);
+
+        drawWalls(width-offset);
+        if (count > 800) {
+            drawWalls(-offset);
+        }
+    }
+
+    void drawWalls(int offset) {
+        for (int i = 0; i < 4; i++) {
+            drawPairOfWalls(offset + 200 * i, i, wallOffset[i]);
+        }
+    }
+
+    void drawPairOfWalls(int xpos, int i, float offset) {
+        pushMatrix();
+        rotate(PI);
+        image(wall, -xpos - 50, -200 - offset);
+        popMatrix();
+        image(wall, xpos, height/2 + 150 + offset);
+        if (xpos == xpos - wall.width && !lose) {
+            score++;
+        }
+        if (xpos > 0 && xpos < width) {
+            walls[i] = new WallPair(xpos, 200 + offset, height/2 + 150 + offset);
+        }
+    }
+
+    void checkTouch(){
+        for (WallPair pair : walls) {
+            if (pair!=null) {
+                if((ypos < pair.y1 && xpos > pair.x && xpos < pair.x+wall.width) || (ypos > pair.y2 && xpos > pair.x && xpos < pair.x+wall.width)){
+                    println(ypos);
+                    lose = true;
+                }
+            }
+        }
+    }
+
+    void reset() {
+        lose = false;
+        count = 300;
+        ypos = 50;
+        score = 0;
+        vy = 0;
+        for (int i = 0; i < wallOffset.length; i++) {
+            wallOffset[i] = random(-100, 100);
+        }
+    }
+
+    public void keyPressed()  {
+        if (!lose) {
+            vy = -6.0f;
+        }
+        else {
+            reset();
+        }
+    }
+
+    public void keyReleased() {
+        if (!lose) {
+            vy = gravity;
+        }
+    }
+
+    class WallPair {
+        float x, y1, y2;
+        WallPair(float x, float y1, float y2){
+            this.x = x;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+        WallPair(){
+            super();
+        }
     }
 }
